bfbbd535eab965b6f8d6737e8aba04950c0a0e1f
Sergio Villavicencio <sergionicolasvillavicencio@gmail.com>
2022-03-15 19:56:56 -0300

    feat: husky eslint prittier

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from "./logo.svg";
-import "./App.scss";
+import React from 'react'
+import logo from './logo.svg'
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>FrontEnd Hackathon</p>
+        <p>FrontEnd Test Husky!</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
