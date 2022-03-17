047f8e247bd666262ac356a41ba31363cd0eb1c4
carlost95 <carlosanibaltejada@gmail.com>
2022-03-17 12:54:19 -0300

    add router page

--- src/routers/appRouter.js
+++ src/routers/appRouter.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { NotFound } from "./../pages/notFound";
-import { LoginPage } from "./../components/Login";
-import {} from "./";
+import NotFound from "./../pages/notFound";
+import LoginPage from "./../pages/loginPage";
+import HomePage from "./../pages/homePage";
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
