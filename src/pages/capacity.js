31352a263cf4ab71ee94456f2c59906ec250771e
Ariel Alejandro Lorenzo Trujillo <AALorenzoTrujillo@teco.com.ar>
2022-03-17 15:19:24 -0300

    add pages

--- src/pages/capacity.js
+++ src/pages/capacity.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import Capacity from "../components/Capacity";
+import Capacity from "../components/Capacity";
 
 export default function capacityPage() {
   return <Capacity />;
