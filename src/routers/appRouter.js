24125f891d972d1149276018cee6eaffebe42d44
Ariel Alejandro Lorenzo Trujillo <AALorenzoTrujillo@teco.com.ar>
2022-03-17 15:42:23 -0300

    add router pages

--- src/routers/appRouter.js
+++ src/routers/appRouter.js
@@ -3,6 +3,10 @@ import useAuth from "../hooks/useAuth";
 import NotFound from "./../pages/notFound";
 import LoginPage from "./../pages/LoginPage";
 import HomePage from "./../pages/homePage";
+import Abouts from "../pages/Abouts";
+import Access from "./../pages/Access";
+import Capacity from "../pages/Capacity";
+
 
 function AppRouter() {
   const { user, isLoading } = useAuth();
@@ -15,6 +19,18 @@ function AppRouter() {
         <Route
           path="/home"
           element={user ? <HomePage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/abouts"
+          element={user ? <Abouts /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/access"
+          element={user ? <Access /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/capacity"
+          element={user ? <Capacity /> : <Navigate to="/" />}
         />
         <Route path="*" element={<NotFound />} />
       </Routes>
