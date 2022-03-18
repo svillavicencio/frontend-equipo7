650908d56f2ccdfb8bfada88793c324121d63396
florvillafane <florencia.a.villafane@gmail.com>
2022-03-18 01:45:44 -0300

    modifield errors console

--- src/components/Capacity/Capacity.js
+++ src/components/Capacity/Capacity.js
@@ -5,11 +5,11 @@ import {Row, Container, Col, Button } from "react-bootstrap";
 import "./Capacity.scss";
 export default function Capacity() {
   return (
-    <Container className="capacity" fluid>
+    <Container className="capacity">
       <Row md={12} className="capacity_titulo">
           <span> <b>Capacitación</b> </span>
        </Row>
-       <Row md={12} className="capacity_sub" fluid>
+       <Row md={12} className="capacity_sub" >
             <span>Conocenos antes de empezar</span>
        </Row>
        <div className="separacion"></div>
@@ -21,21 +21,21 @@ export default function Capacity() {
              <span className="titul">Seguridad y Higiene</span>
 
           <div className="capacity_contenedor">
-          <iframe width="280" height="200" src="https://www.youtube.com/embed/xkKwLq8rR8Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="280" height="200" src="https://www.youtube.com/embed/xkKwLq8rR8Y" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           </div>
           <Button className="capacity_subtitulo">Comenzar Curso</Button>
         </Col>
         <Col sm={11} md={3} className="capacity_ext">
         <span className="titul">Ética</span>
           <div className="capacity_contenedor">
-          <iframe width="280" height="200" src="https://www.youtube.com/embed/9n40IM8IFK0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="280" height="200" src="https://www.youtube.com/embed/9n40IM8IFK0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           </div>
           <Button className="capacity_subtitulo">Comenzar Curso</Button>
          </Col>
         <Col sm={11} md={3} className="home_ext">
         <span className="titul">Licencias</span>
           <div className="capacity_contenedor">
-          <iframe width="280" height="200" src="https://www.youtube.com/embed/7laodN5DoT8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="280" height="200" src="https://www.youtube.com/embed/7laodN5DoT8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             
               </div>
               <Button className="capacity_subtitulo">Comenzar Curso</Button>
